d1e27e17dfe647851d706b2bf9b675ea56c66c3d
Benjamin Bauer <benjamin.bauer@stud.h-da.de>
2015-02-15 17:18:42 +0100

    Walkthrough fertig

--- doku/js/custom/walkthrough.js
+++ doku/js/custom/walkthrough.js
@@ -2,10 +2,37 @@ var wtfunction = function(){
 
 	$(".btnradarmit").click(function(){
 		var currentstate = $(".wt-active");
-		console.log("WAAAAAAAASSSSSS");
 
 		currentstate.removeClass('wt-active');
 		$('#radarmit').addClass('wt-active');
+	});
+
+	$(".btnschreiben").click(function(){
+		var currentstate = $(".wt-active");
+
+		currentstate.removeClass('wt-active');
+		$('#stateschreiben').addClass('wt-active');
+	});
+
+	$(".btnradarohne").click(function(){
+		var currentstate = $(".wt-active");
+
+		currentstate.removeClass('wt-active');
+		$('#radarohne').addClass('wt-active');
+	});
+
+	$(".btnlesen").click(function(){
+		var currentstate = $(".wt-active");
+
+		currentstate.removeClass('wt-active');
+		$('#statelesen').addClass('wt-active');
+	});
+
+	$(".btnzeitstrahl").click(function(){
+		var currentstate = $(".wt-active");
+
+		currentstate.removeClass('wt-active');
+		$('#statezeitstrahl').addClass('wt-active');
 	});
 
 };
